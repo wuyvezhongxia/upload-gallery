655f9b3e580eece08c7100f6547df91e1c58824b
May <536440449@qq.com>
2025-07-08 09:14:21 +0800

    fix: 修改配置的路径别名

--- packages/upload/src/pages/header/index.tsx
+++ packages/upload/src/pages/header/index.tsx
@@ -1,6 +1,5 @@
 import './index.scss'
 import { Button } from 'antd'
-import '../../assets/logo.svg'
 import GenerateToken from '../generate-token/index'
 import {useState} from 'react'
 const Header = ()=>{
@@ -9,12 +8,14 @@ const Header = ()=>{
       <>
         <div className="header">
           <div className="left">
-            <img src="/assets/logo.svg" alt="" />
+            <img src="../../assets/logo.svg" alt="" />
             <h3>源境静态资源管理平台</h3>
           </div>
-          <Button className="right iconfont" onClick={()=>setShow(true)}>&#xe653;</Button>
+          <Button className="right iconfont" onClick={() => setShow(true)}>
+            &#xe653;
+          </Button>
         </div>
-        <GenerateToken show={show} setShow={setShow}> </GenerateToken>
+        <GenerateToken show={show} setShow={setShow}></GenerateToken>
       </>
     );
 }
