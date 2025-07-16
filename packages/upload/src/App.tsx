9c50e9e018ff09266265948868e0cf836725209c
May <536440449@qq.com>
2025-07-16 20:47:49 +0800

    feat: 本地只存近七天的内容

--- packages/upload/src/App.tsx
+++ packages/upload/src/App.tsx
@@ -1,8 +1,27 @@
-import Header from "./pages/header"
-import UploadFile from "./pages/upload-file"
-import FileList from "./pages/picture-list"
-import UploadTool from "./pages/upload-tool"
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { cleanExpiredImages } from "./store/modules/imageStore";
+import Header from "./pages/header";
+import UploadFile from "./pages/upload-file";
+import FileList from "./pages/picture-list";
+import UploadTool from "./pages/upload-tool";
+
 function App() {
+  const dispatch = useDispatch();
+
+  // 在应用启动时清理过期图片
+  useEffect(() => {
+    dispatch(cleanExpiredImages());
+  }, [dispatch]);
+
+  // 定期清理（可选）
+  useEffect(() => {
+    const interval = setInterval(() => {
+      dispatch(cleanExpiredImages());
+    }, 24 * 60 * 60 * 1000); // 每24小时清理一次
+
+    return () => clearInterval(interval);
+  }, [dispatch]);
 
   return (
     <>
@@ -14,4 +33,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
